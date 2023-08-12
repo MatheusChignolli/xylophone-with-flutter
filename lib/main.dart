a43b3560f9c8ebaece38a05f9355c25a412820a3
Matheus Chignolli <matheuschignolli@gmail.com>
2023-08-12 12:42:30 -0300

    improve colors

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class XylophoneApp extends StatelessWidget {
 
     const keys = [
       {'key': 1, 'backgroundColor': Colors.red},
-      {'key': 2, 'backgroundColor': Colors.blue},
-      {'key': 3, 'backgroundColor': Colors.pink},
-      {'key': 4, 'backgroundColor': Colors.purple},
-      {'key': 5, 'backgroundColor': Colors.yellow},
-      {'key': 6, 'backgroundColor': Colors.green},
-      {'key': 7, 'backgroundColor': Colors.orange},
+      {'key': 2, 'backgroundColor': Colors.orange},
+      {'key': 3, 'backgroundColor': Colors.yellow},
+      {'key': 4, 'backgroundColor': Colors.green},
+      {'key': 5, 'backgroundColor': Colors.blueGrey},
+      {'key': 6, 'backgroundColor': Colors.blue},
+      {'key': 7, 'backgroundColor': Colors.purple},
     ];
 
     void _onPressed(int key) async {
@@ -30,22 +30,24 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: keys
-                .map((key) => Expanded(
-                      child: FilledButton(
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(0),
-                            ),
-                          ),
-                          backgroundColor: MaterialStateProperty.all(
-                            key['backgroundColor'],
+                .map(
+                  (key) => Expanded(
+                    child: FilledButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
                           ),
                         ),
-                        onPressed: () => _onPressed(key['key']),
-                        child: Text(''),
+                        backgroundColor: MaterialStateProperty.all(
+                          key['backgroundColor'],
+                        ),
                       ),
-                    ))
+                      onPressed: () => _onPressed(key['key']),
+                      child: Text(''),
+                    ),
+                  ),
+                )
                 .toList(),
           ),
         ),
